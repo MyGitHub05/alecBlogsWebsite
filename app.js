461db0330fbac34aaa7cee5263687db496aa922b
alecdev <alecvictorio2@gmail.com>
2023-11-28 16:47:58 +0800

    use middleware to use static files like stylesheet style.css, put some style in nav

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+//middle ware
+const morgan = require("morgan");
 
 const app = express();
 
@@ -7,6 +9,12 @@ app.set("view engine", "ejs");
 
 //listen
 app.listen(3000);
+
+//middle ware & static files
+//yung express.static middle ware sya na ginagamit para sa mga static files yung public naman yan yung folder na papayagan niya pumasok sa ating mga files
+app.use(express.static("public"));
+//use morgan middle ware
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Home page" });
